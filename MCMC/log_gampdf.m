8d8a1b2f752a37a5f393f73329b8d9855510a7a7
deniellericciardi <ricciardi.24@osu.edu>
2019-10-24 15:44:01 -0400

    deleted duplicat directories

--- MCMC/log_gampdf.m
+++
@@ -1,7 +0,0 @@
-function log_gampdf = log_gampdf(x,shape,rate)
-%un-normalized gamma pdf
-%a_tau = shape, b_tau = rate
-
-log_gampdf = (shape - 1)*log(x) - (x*rate);
-
-end
--- MCMC/log_normal.m
+++
@@ -1,4 +0,0 @@
-function log_norm = log_normal(x,mu,variance)
-%log normal up to a constant of proportionality
-    log_norm = -((x-mu).^2)./(2.*variance);
-end
